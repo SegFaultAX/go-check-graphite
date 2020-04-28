328d88a85f339a6b24752daf0e0288b7e211a892
Judson <nyarly@gmail.com>
2020-04-28 10:10:40 -0700

    Refactoring table conditions out of tests

--- aggregations_test.go
+++ aggregations_test.go
@@ -1,14 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"testing"
-)
-
-var (
-	noNulls   = floats(1.1, 2.2, 3.3)
-	someNulls = floats(1.1, nil, 3.3)
-	allNulls  = floats(nil, nil, nil)
 )
 
 type ex struct {
@@ -27,42 +22,93 @@ type example struct {
 	all ex
 }
 
+var samples = map[string][]*float64{
+  "noNulls":    floats(1.1, 2.2, 3.3),
+  "someNulls":  floats(1.1, nil, 3.3),
+  "allNulls":   floats(nil, nil, nil),
+}
+
 // for each aggregation, we define 3 examples:
 // none - what the aggregation should do when there are no nulls
 // some - what the aggregation should do when there are some nulls
 // all - what the aggregation should do when there are only nulls
 // n.b., use coverage to ensure all aggregations are tested thoroughly
-var examples = map[string]example{
-	"sum":     {none: ex{false, 6.6}, some: ex{false, 4.4}, all: ex{true, 0.0}},
-	"avg":     {none: ex{false, 6.6 / 3}, some: ex{false, 4.4 / 2}, all: ex{true, 0.0}},
-	"min":     {none: ex{false, 1.1}, some: ex{false, 1.1}, all: ex{true, 0.0}},
-	"max":     {none: ex{false, 3.3}, some: ex{false, 3.3}, all: ex{true, 0.0}},
-	"median":  {none: ex{false, 2.2}, some: ex{false, 3.3}, all: ex{true, 0.0}},
-	"95th":    {none: ex{false, 3.3}, some: ex{false, 3.3}, all: ex{true, 0.0}},
-	"99th":    {none: ex{false, 3.3}, some: ex{false, 3.3}, all: ex{true, 0.0}},
-	"999th":   {none: ex{false, 3.3}, some: ex{false, 3.3}, all: ex{true, 0.0}},
-	"nullcnt": {none: ex{false, 0.0}, some: ex{false, 1.0}, all: ex{false, 3.0}},
-	"nullpct": {none: ex{false, 0.0}, some: ex{false, 1.0 / 3.0}, all: ex{false, 1.0}},
+var examples = []struct{
+  agg string; sample string; expected float64 }{
+	{"avg",      "noNulls",    6.6 / 3,    },
+	{"avg",      "someNulls",  4.4 / 2,    },
+	{"max",      "noNulls",    3.3,        },
+	{"max",      "someNulls",  3.3,        },
+	{"median",   "noNulls",    2.2,        },
+	{"median",   "someNulls",  3.3,        },
+	{"min",      "noNulls",    1.1,        },
+	{"min",      "someNulls",  1.1,        },
+	{"nullcnt",  "allNulls",   3.0,        },
+	{"nullcnt",  "noNulls",    0.0,        },
+	{"nullcnt",  "someNulls",  1.0,        },
+	{"nullpct",  "allNulls",   1.0,        },
+	{"nullpct",  "noNulls",    0.0,        },
+	{"nullpct",  "someNulls",  1.0 / 3.0,  },
+	{"sum",      "noNulls",    6.6,        },
+	{"sum",      "someNulls",  4.4,        },
+	{"95th",     "noNulls",    3.3,        },
+	{"95th",     "someNulls",  3.3,        },
+	{"999th",    "noNulls",    3.3,        },
+	{"999th",    "someNulls",  3.3,        },
+	{"99th",     "noNulls",    3.3,        },
+	{"99th",     "someNulls",  3.3,        },
 }
 
-func TestAggregations(t *testing.T) {
-	for agg, ex := range examples {
-		testSample(t, agg, "noNulls", noNulls, ex.none)
-		testSample(t, agg, "someNulls", someNulls, ex.some)
-		testSample(t, agg, "allNulls", allNulls, ex.all)
+
+var errExamples = []struct{
+  agg string; sample string }{
+	{"avg",     "allNulls",  },
+	{"max",     "allNulls",  },
+	{"median",  "allNulls",  },
+	{"min",     "allNulls",  },
+	{"sum",     "allNulls",  },
+	{"95th",    "allNulls",  },
+	{"999th",   "allNulls",  },
+	{"99th",    "allNulls",  },
+}
+
+func TestAggregationsWithoutErrors(t *testing.T) {
+	for _, ex := range examples {
+		testNoError(t, ex.agg, ex.sample, samples[ex.sample], ex.expected)
 	}
 }
 
-func testSample(t *testing.T, agg string, name string, vs []*float64, e ex) {
-	f := aggregations[agg]
-	res, err := f(vs)
-	if err != nil && !e.err {
-		t.Errorf("unexpected error for %s on %s: %s", agg, name, err)
-	} else if err == nil && e.err {
-		t.Errorf("expected error for %s on %s, but got result %f", agg, name, res)
-	} else if !eq(e.result, res) {
-		t.Errorf("expected %f for %s on %s, but got %f", e.result, agg, name, res)
+func TestAggregationsWithErrors(t *testing.T) {
+	for _, ex := range errExamples {
+		testExpectError(t, ex.agg, ex.sample, samples[ex.sample])
 	}
+}
+
+func testNoError(t *testing.T, agg string, name string, vs []*float64, expected float64) {
+	t.Run(fmt.Sprintf("%s - %s expecting no error", agg, name), func(t *testing.T) {
+		f := aggregations[agg]
+		actual, err := f(vs)
+		if err != nil {
+			t.Errorf("unexpected error for %s on %s: %s", agg, name, err)
+		}
+    if !eq(expected, actual) {
+			t.Errorf("expected %f for %s on %s, but got %f", expected, agg, name, actual)
+		}
+	})
+}
+
+func testExpectError(t *testing.T, agg string, name string, vs []*float64) {
+	t.Run(fmt.Sprintf("%s - %s, expecting error", agg, name), func(t *testing.T) {
+    expected := 0.0
+		f := aggregations[agg]
+		actual, err := f(vs)
+    if err == nil {
+			t.Errorf("expected error for %s on %s, but got result %f", agg, name, actual)
+		}
+    if !eq(expected, actual) {
+			t.Errorf("expected %f for %s on %s, but got %f", expected, agg, name, actual)
+		}
+	})
 }
 
 func eq(a float64, b float64) bool {
@@ -70,14 +116,14 @@ func eq(a float64, b float64) bool {
 }
 
 func floats(vs ...interface{}) []*float64 {
-	res := make([]*float64, 0, len(vs))
+	actual := make([]*float64, 0, len(vs))
 	for _, v := range vs {
 		if v == nil {
-			res = append(res, nil)
+			actual = append(actual, nil)
 		} else {
 			f := v.(float64)
-			res = append(res, &f)
+			actual = append(actual, &f)
 		}
 	}
-	return res
+	return actual
 }
